ac9124e6bef83964eec6aa2813b879f795b22008
Prabhat <prabhatmani933@gmail.com>
2022-04-27 01:05:55 +0530

    Added register and login

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const express = require('express')
 const app = express()
 const User = require('./models/user')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+mongoose
+  .connect('mongodb://0.0.0.0:27017/authNoSql')
+  .then(() => {
+    console.log('MONNGO CONNECTION OPEN!!!')
+  })
+  .catch((err) => {
+    console.log('OH NO MONGO ERROR!!!!')
+    console.log(err)
+  })
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Home Page!')
+})
+
 app.get('/register', (req, res) => {
   res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  const hash = await bcrypt.hash(password, 12)
+  const user = new User({
+    username: username,
+    password: hash,
+  })
+  await user.save()
+  res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  const user = await User.findOne({ username })
+  const validPassword = await bcrypt.compare(password, user.password)
+  if (validPassword) {
+    res.send('YAY Welcome!')
+  } else {
+    res.send('Try Again!')
+  }
 })
 
 app.get('/secret', (req, res) => {
--- models/user.js
+++ models/user.js
@@ -11,4 +11,4 @@ const userSchema = new mongoose.Schema({
   },
 })
 
-module.export = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
