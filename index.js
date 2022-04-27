a270a8d8af2f3c285376404346d0417f1948bc2e
Prabhat <prabhatmani933@gmail.com>
2022-04-28 03:11:21 +0530

    Added remember when logged in feature!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const User = require('./models/user')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const session = require('express-session')
 
 mongoose
   .connect('mongodb://0.0.0.0:27017/authNoSql')
@@ -18,6 +19,14 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'notagoodsecret' }))
+
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect('login')
+  }
+  next()
+}
 
 app.get('/', (req, res) => {
   res.send('Home Page!')
@@ -35,6 +44,7 @@ app.post('/register', async (req, res) => {
     password: hash,
   })
   await user.save()
+  req.session.user_id = user._id
   res.redirect('/')
 })
 
@@ -47,14 +57,24 @@ app.post('/login', async (req, res) => {
   const user = await User.findOne({ username })
   const validPassword = await bcrypt.compare(password, user.password)
   if (validPassword) {
-    res.send('YAY Welcome!')
+    req.session.user_id = user._id
+    res.redirect('/secret')
   } else {
-    res.send('Try Again!')
+    res.redirect('/login')
   }
 })
 
-app.get('/secret', (req, res) => {
-  res.send('This is a secret! U cant see it unless logged in.')
+app.post('/logout', (req, res) => {
+  req.session.user_id = null
+  res.redirect('/login')
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+  res.render('secrets')
+})
+
+app.get('/topsecret', requireLogin, (req, res) => {
+  res.send('Top Secret!!')
 })
 
 app.listen(5000, () => {
